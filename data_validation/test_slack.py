4f59f871f37c870980087ae5a969b05fd71bc362
Haneul Kim <ttcielott@gmail.com>
2023-07-20 20:38:09 +0100

    fixed system argument part

--- data_validation/test_slack.py
+++ data_validation/test_slack.py
@@ -7,10 +7,11 @@ parser = configparser.ConfigParser()
 parser.read("../pipeline.conf")
 webhook_url = parser.get('slack_notification', 'webhook_url')
 
-test_result = sys.argv[1]
-script_1 = sys.argv[2]
-script_2 = sys.argv[3]
-comp_operator = sys.argv[4]
+
+script_1 = sys.argv[1]
+script_2 = sys.argv[2]
+comp_operator = sys.argv[3]
+test_result = sys.argv[4]
 
 if test_result == True: 
     exit(0)
